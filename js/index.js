cbc551b941acbddddbb06318428d9e2c6b50b2de
NineChu <ninechu0@gmail.com>
2026-06-08 01:08:53 -0300

    feat: adding localstorage memory

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 /** @type {HTMLTextAreaElement} */
 const textareaEditor = document.getElementById("textareaEditor");
+/** @type {HTMLButtonElement} */
+const buttonSettings = document.getElementById("buttonSettings");
+/** @type {HTMLDialogElement} */
+const dialogSettings = document.getElementById("dialogSettings");
+/** @type {HTMLButtonElement} */
+const buttonCloseSettings = document.getElementById("buttonCloseSettings");
+/** @type {HTMLInputElement} */
+const inputLocalstorageSetting = document.getElementById("inputLocalstorageSetting");
+
+if (localStorage.length > 0) {
+  textareaEditor.value = localStorage.getItem(`textareaEditor`);
+  inputLocalstorageSetting.checked = true;
+}
+
+buttonSettings.addEventListener(`click`, (event) => {
+  dialogSettings.showModal();
+});
+buttonCloseSettings.addEventListener(`click`, (event) => {
+  dialogSettings.close();
+});
+
+window.addEventListener(`beforeunload`, (event) => {
+  if (inputLocalstorageSetting.checked) {
+    localStorage.setItem(`textareaEditor`, textareaEditor.value);
+  } else if (localStorage.length > 0) {
+    localStorage.clear();
+  }
+});
